5cefc114ca75b8552fcc08c2d3b0eab77d21489b
Sara Valdéz <24sara.valdez@gmail.com>
2026-06-01 13:30:35 -0600

    Antes de que se vaya la luz

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let contenedorCartas = document.querySelector('.contenedor-cartas')
 let botonAgregar = document.querySelector('.botonAgregar')
 let input = document.querySelector('.ingreso')
-let contadorPeliculas = 0
 let peliculas = []
 let cuandoNoHayPeliclas = document.querySelector('.cuandoNoHayPeliculas')
 let savedVotes = 0
+let lugardelTopFilm = document.querySelector('#topFilm')
 
 let topFilm = ''
 let mayor = 0
@@ -13,14 +13,12 @@ let mayor = 0
 
 
 const peliculaPorAgregar = () => {
-    contadorPeliculas++
     let tarjeta = document.createElement('div')
     tarjeta.classList.add('tarjetita')
 
     let nombrePelicula = document.createElement('p')
     nombrePelicula.classList.add('nombrePelicula')
     nombrePelicula.textContent = input.value
-    //  nombrePelicula.setAttribute('id', contadorPeliculas)
 
     let cantidadVotos = document.createElement('p')
     cantidadVotos.classList.add('cantidadVotos')
@@ -42,7 +40,6 @@ const peliculaPorAgregar = () => {
 
     contenedorCartas.append(tarjeta)
 
-    //contadorPeliculas++
 
     input.value = ''
     let contador = 0
@@ -60,25 +57,23 @@ const peliculaPorAgregar = () => {
         let datosFilm = {
             nombre: nombrePelicula.textContent,
             votos: cantidadVotos.textContent[cantidadVotos.textContent.length - 1],
-            id: contadorPeliculas
         }
-
-        // peliculas = []
         peliculas.push(datosFilm)
 
         console.log(peliculas)
 
         peliculas.forEach((elemento) => {
-            //console.log('yo soy el primer eleento de pas', elemento.votos)
             for (let i = 0; i < peliculas.length; i++) {
                 if (i == 0) {
                     topFilm = peliculas[i].nombre
                     mayor = peliculas[i].votos
+                    lugardelTopFilm.textContent = 'The #1 Film is: ' + topFilm
                 } else {
                     if (peliculas[i].votos > mayor && peliculas[i].nombre != topFilm) {
                         topFilm = peliculas[i].nombre
                         mayor = peliculas[i].votos
-                        console.log('yo sere el top film escogido', topFilm, mayor)
+                        lugardelTopFilm.textContent = 'The #1 Film is: ' + topFilm
+
                     }
                 }
 
@@ -86,7 +81,6 @@ const peliculaPorAgregar = () => {
         })
 
     })
-
 
 
 
@@ -105,14 +99,3 @@ botonAgregar.addEventListener('click', () => {
 })
 
 
-
-
-// // for(let i = 0; i < peliculas.length; i++){
-// //     if(i == 0){
-//         topFilm = peliculas[0].nombre
-//         mayor = peliculas[0].votos
-
-//         console.log(topFilm)
-//         console.log(mayor)
-// //     }
-// // }
